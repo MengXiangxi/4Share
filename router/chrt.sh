5b6e709c7f8f08aa4ed20aac441ce1facb7f13f1
IceCodeNew <32576256+IceCodeNew@users.noreply.github.com>
2018-05-12 02:16:12 +0800

    Bug fix & Update

--- router/chrt.sh
+++ router/chrt.sh
@@ -1,29 +1,31 @@
 #!/bin/sh
 
+script_path="/tmp/mnt/router/configs"
+
 if [ $# -ne 1 ]; then
     echo $0 add/del/upd
     exit
 fi
 
 if [ "$1" == "add" ]; then
-    chmod a+rx /tmp/mnt/router/configs/*
+    chmod a+rx ${script_path}/*
 
-    /tmp/mnt/router/configs/route.sh add && echo -e "\n  Add route success!\n"
+    "${script_path}/route.sh" add && echo -e "\n  Add route success!\n"
 elif [ "$1" == "del" ]; then
-    chmod a+rx /tmp/mnt/router/configs/*
+    chmod a+rx ${script_path}/*
 
-    /tmp/mnt/router/configs/route.sh delete && echo -e "\n  Delete route success!\n"
-elif [ "$1" != "upd" ]; then
-    curl -o /tmp/mnt/router/configs/route.sh.new https://raw.githubusercontent.com/IceCodeNew/4Share/master/router/route.sh \
-    && mv /tmp/mnt/router/configs/route.sh.new /tmp/mnt/router/configs/route.sh -f && echo -e "\n  Update route.sh success!\n"
+    "${script_path}/route.sh" delete && echo -e "\n  Delete route success!\n"
+elif [ "$1" == "upd" ]; then
+    curl -o "${script_path}/route.sh.new" https://raw.githubusercontent.com/IceCodeNew/4Share/master/router/route.sh \
+    && mv "${script_path}/route.sh.new" "${script_path}/route.sh" -f && echo -e "\n  Update route.sh success!\n"
 
-    curl -o /tmp/mnt/router/configs/accelerated-domains.china.conf.new \
+    curl -o "${script_path}/accelerated-domains.china.conf.new" \
     https://raw.githubusercontent.com/IceCodeNew/4Share/master/router/accelerated-domains.china.conf && \
-    mv /tmp/mnt/router/configs/accelerated-domains.china.conf.new /tmp/mnt/router/configs/accelerated-domains.china.conf -f \
+    mv "${script_path}/accelerated-domains.china.conf.new" "${script_path}/accelerated-domains.china.conf" -f \
     && echo -e "\n  Update accelerated-domains.china.conf success!\n"
+
+    service restart_dnsmasq && echo -e "\n  Restart dnsmasq success!\n"
 else
     echo $0 add/del/upd
     exit
 fi
-
-service restart_dnsmasq && echo -e "\n  Restart dnsmasq success!\n"

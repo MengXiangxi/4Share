44a34bd3717544ecb39e7883feeb6363161d8ef5
IceCodeNew <32576256+IceCodeNew@users.noreply.github.com>
2018-05-24 00:56:26 +0800

    Update

--- router/route.sh
+++ router/route.sh
@@ -21,11 +21,11 @@ OPS=$1
 
 # route $OPS -net ${IP_SEGMENT} ${ROUTE_GW}
 # Generate:
-# https://raw.githubusercontent.com/17mon/china_ip_list/master/china_ip_list.txt
+# wget https://raw.githubusercontent.com/17mon/china_ip_list/master/china_ip_list.txt
 # sed -i -e 's/^/route\ \${OPS}\ -net\ &/g' -e 's/$/&\ \${ROUTE_GW}/g' china_ip_list.txt
 
 # 另一边要用到的命令：
-# https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/accelerated-domains.china.conf
+# wget https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/accelerated-domains.china.conf
 # sed -i 's/114.114.114.114/119.29.29.29/g' accelerated-domains.china.conf
 
 route ${OPS} -net 1.0.1.0/24 ${ROUTE_GW}
